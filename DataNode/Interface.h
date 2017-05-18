9fe0482df3cb56107f64d49382400d43e2d45a6e
barry.paneer <barry.paneer@qq.com>
2017-05-18 18:17:02 +0800

    updates

--- DataNode/Interface.h
+++ DataNode/Interface.h
@@ -64,6 +64,14 @@ public:
 
 
 
+typedef int					__stdcall T_Func_Initialize( I_DataHandle* pIDataHandle );
+typedef void				__stdcall T_Func_Release();
+typedef int					__stdcall T_Func_RecoverQuotation();
+typedef int					__stdcall T_Func_GetStatus();
+
+
+
+
 #endif
 
 
